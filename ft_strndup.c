e12fb6b9b48a92e62735abfd10b7c9795687e4b3
Thibault BERTHIER <tberthie@e2r9p17.42.fr>
2017-02-22 15:48:32 +0100

    Fix strndup

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/22 14:46:38 by tberthie          #+#    #+#             */
-/*   Updated: 2017/02/22 14:46:56 by tberthie         ###   ########.fr       */
+/*   Updated: 2017/02/22 15:48:09 by tberthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*ft_strndup(const char *str, int size)
 	char	*dup;
 
 	dup = (char*)ft_m(sizeof(char) * (size + 1));
-	ft_strcpy(dup, str);
+	while (size--)
+		dup[size] = str[size];
 	return (dup);
 }
